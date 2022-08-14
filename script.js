bb67b6a8fd30b41fc1e1f017b5ecf0063d064ee0
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2022-08-14 16:16:37 -0300

    resp

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function juntarTextos(){
-    
+function juntarTextos() {
+  const entrada1 = document.getElementById("entrada1");
+  const entrada2 = document.getElementById("entrada2");
+  const entrada3 = document.getElementById("entrada3");
+  const vetor = [entrada1.value, entrada2.value, entrada3.value];
+  let resposta = join(vetor);
+  alert(resposta);
+}
+
+function join(vetor) {
+  let resposta = "";
+  for (let index = 0; index < vetor.length; index++) {
+    resposta += concat(index, vetor[index], vetor.length);
+  }
+  return resposta;
+}
+
+function concat(index, element, length) {
+  let resposta = "";
+  resposta += element;
+  if (index + 1 < length) {
+    resposta += ",";
+  }
+  return resposta;
 }
