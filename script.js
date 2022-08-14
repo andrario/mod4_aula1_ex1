dfd75e921a04382c9278b2bbcfd52e2e139db92d
7cristovao <cristovao.rocha@gmail.com>
2022-08-14 16:19:04 -0300

    primeira pratica

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const elements = [];
+
 function juntarTextos(){
-    
+    let ent1 = document.getElementById("entrada1").value
+    let ent2 = document.getElementById("entrada2").value
+    let ent3 = document.getElementById("entrada3").value
+
+    elements.push(ent1,ent2,ent3)
+
+    console.log(elements.join())
+    console.log(elements.join(''))
+    console.log(elements.join('-'))
 }
+
