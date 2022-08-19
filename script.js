9550d709988d04ffbd7a98fc6aa97bbf52df95f8
andrario <andre.mario.r.santos@gmail.com>
2022-08-18 22:38:33 -0300

    resolução

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function juntarTextos(){
-    
+    let lista = []
+    lista[0] = document.getElementById("entrada1").value
+    lista[1] = document.getElementById("entrada2").value
+    lista[2] = document.getElementById("entrada3").value
+
+    document.getElementById("resposta").textContent = lista.join("-")
 }
